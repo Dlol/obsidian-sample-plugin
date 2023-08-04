4c249268ee0df6fa04e3a8921824c4ee70bf7236
Dlol <dravennadas@gmail.com>
2023-08-03 23:52:33 -0500

    options (real, final, for real this time)

--- MainView.ts
+++ MainView.ts
@@ -7,12 +7,10 @@ export const VIEW_TYPE_EXAMPLE = "example-view";
 export class ExampleView extends ItemView {
     component: Component;
     questionData: QuestionData;
-    reloadData: Function
 
     constructor(leaf: WorkspaceLeaf, questionData: QuestionData) {
         super(leaf);
         this.questionData = questionData
-        this.reloadData = reloadData
     }
 
     getViewType() {
